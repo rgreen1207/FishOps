6a3d592c5419139c0e349bd6525c71ef0c09b8b7
Gregory Greenleaf <pacohojaverde@gmail.com>
2016-04-23 03:57:46 -0700

    Implemented clickable polys, error still in Maps activity, will work on soon!

--- app/src/main/java/us/fishops/android/fishops/MapsActivity.java
+++ app/src/main/java/us/fishops/android/fishops/MapsActivity.java
@@ -1,18 +1,24 @@
 package us.fishops.android.fishops;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import java.util.HashMap;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private HashMap<Polygon, String> hMap = new HashMap<Polygon, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    public static int getColorWithAlpha(int color, float ratio){
+        int newColor = 0;
+        int alpha = Math.round(Color.alpha(color) * ratio);
+        int r = Color.red(color);
+        int g = Color.green(color);
+        int b = Color.blue(color);
+        newColor = Color.argb(alpha, r, g, b);
+        return newColor;
+    }
+
+    // Pass an array of Lat/Long and String name
+    private void createPoly(String args[], String name){
+        Polygon polygon = mMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(14.675757,120.619596),new LatLng(14.655245,120.892407),new LatLng(14.517079,120.903715),new LatLng(14.506132,120.647866))
+                .strokeColor(Color.RED)
+                .strokeWidth(3)
+                .fillColor(getColorWithAlpha(Color.RED, 0.4f)));
+        polygon.setClickable(true);
+    }
 
     /**
      * Manipulates the map once available.
@@ -42,6 +67,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng manila = new LatLng(14.5995, 120.9842);
         mMap.addMarker(new MarkerOptions().position(manila).title("Marker in Manila"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(manila));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(14.5995, 120.9842),17.0f));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(14.5995, 120.9842),9.0f));
+
+        createPoly();
+
+        googleMap.setOnPolygonClickListener(new GoogleMap.OnPolygonClickListener() {
+            @Override
+            public void onPolygonClick(Polygon polygon) {
+                Toast.makeText(MapsActivity.this, "Test", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
