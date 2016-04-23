c5b9ef0e745c032ae240328de50b949948a75caa
Brian Huynh <brianhuynh1@gmail.com>
2016-04-23 11:06:59 -0700

    Finished landing screen

--- app/src/main/java/us/fishops/android/fishops/MainActivity.java
+++ app/src/main/java/us/fishops/android/fishops/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(R.string.app_name);
         setContentView(R.layout.activity_main);
     }
 
