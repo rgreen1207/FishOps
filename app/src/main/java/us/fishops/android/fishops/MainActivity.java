ffca0435086270d84b643a888e13e71dd83c6eff
Gregory Greenleaf <pacohojaverde@gmail.com>
2016-04-23 20:58:05 -0700

    Commit before merging into my own branch.

--- app/src/main/java/us/fishops/android/fishops/MainActivity.java
+++ app/src/main/java/us/fishops/android/fishops/MainActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.zetterstrom.com.forecast.ForecastClient;
+import android.zetterstrom.com.forecast.ForecastConfiguration;
+import android.zetterstrom.com.forecast.models.Forecast;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
     public void goToMap(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+    }
+
+    public void getWeather(final double latitude, final double longitude){
+        //getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_framelayout, new SnowFragment()).commit();
+        ForecastConfiguration configuration = new ForecastConfiguration.Builder("4c53088bf37f39b40f21165b81d5b69f")
+            .setCacheDirectory(getCacheDir()).build();
+        ForecastClient.create(configuration);
+        ForecastClient.getInstance().getForecast(latitude, longitude, new Callback<Forecast>(){
+            @Override
+            public void onResponse(Response<Forecast> response){
+                if (response.isSuccess()){
+                    Forecast forecast = response.body();
+                }
+            }
+            @Override
+            public void onFailure(Throwable t){
+                t.printStackTrace();
+            }
+        });
     }
 }
