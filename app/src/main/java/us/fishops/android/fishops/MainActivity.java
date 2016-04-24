c1e46eb2e7c48e2f44100981df1d1002cb6801a2
Zac Leids <zacleids@aol.com>
2016-04-23 20:45:36 -0700

    first commit to zac

--- app/src/main/java/us/fishops/android/fishops/MainActivity.java
+++ app/src/main/java/us/fishops/android/fishops/MainActivity.java
@@ -1,5 +1,7 @@
 package us.fishops.android.fishops;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+        builder1.setMessage("You have entered a Marine Protected Area");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder1.create();
+        alert.show();
     }
 
     public void goToMap(View view) {
--- app/src/main/java/us/fishops/android/fishops/MapsActivity.java
+++ app/src/main/java/us/fishops/android/fishops/MapsActivity.java
@@ -1,26 +1,43 @@
 package us.fishops.android.fishops;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.maps.android.PolyUtil;
 
 import java.io.*;
 
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -29,11 +46,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private HashMap<Polygon, String> hMap = new HashMap<Polygon, String>();
     private final static String TAG = "j";
     private GoogleApiClient mGoogleApiClient;
+    private static final int REQUEST_CODE_LOCATION = 2;
     //File MPA = new File("../../../../../res/PN_MPAs.txt");
 
     ///home/ryan/Desktop/FishOps/app/src/main/java/us/fishops/android/fishops/MapsActivity.java
     ///home/ryan/Desktop/FishOps/app/src/main/res/PN_MPAs
     String line = null;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +72,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //                .addOnConnectionFailedListener(this)
 //                .addApi(LocationServices.API)
 //                .build();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mClient = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public static int getColorWithAlpha(int color, float ratio) {
@@ -83,58 +109,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void parse() {
-        String name, cords, blankLine;
-//        String inputFile = "raw/t.txt";
-//        File file = new File(inputFile);
-        // LOCATION OF TEXT FILE
-//        String s1 = "122.152777778000086,13.372777778000057 122.170277778000013,13.35444444400008 122.198611111000105,13.330555556000093 122.172222222000073,13.321944444000053 122.172222222000073,13.306388889000061 122.163888889000077,13.295 122.139444444000105,13.286111111000082 122.093888889000027,13.328055556000038 122.152777778000086,13.372777778000057";
-//        String n1 = "Torrijos Community";
-//        points.clear();
-//        points = generatePoints(s1);
-//        //Log.i(TAG, points.toString());
-//        createPoly(points,n1);
-//        String s2 = "123.775,13.416833333000056 123.798,13.413833333000071 123.795166667000103,13.40716666700007 123.772333333,13.410166667000055 123.775,13.416833333000056";
-//        String n2 = "San Miguel Island";
-//        points.clear();
-//        points = generatePoints(s2);
-//        //Log.i(TAG, points.toString());
-//        createPoly(points,n2);
-//        String s3 = "124.927600065000092,10.649884265000082 124.928591874000062,10.646550020000063 124.928736831000037,10.64454623000006 124.928851271000099,10.642309730000079 124.929598943000087,10.640236318000063 124.931246874000067,10.638558705000037 124.934237565000103,10.63569846300004 124.935809203000076,10.634299338000062 124.937403730000028,10.632900215000063 124.938525238000011,10.63111673700007 124.938883816000043,10.629003268000076 124.93853286600006,10.626823992000084 124.937830969000061,10.62463327100005 124.93724351000003,10.622377696000058 124.936518725000042,10.619754936000049 124.934397776000083,10.620157411000037 124.932292084000096,10.620271860000059 124.93027031500003,10.61991802600005 124.928782599000101,10.621452581000085 124.927714496000021,10.623301865000087 124.928614756,10.625428686000078 124.928065446,10.627824460000056 124.927340661000017,10.629768164000041 124.927989154000102,10.63177767500008 124.927119413000014,10.634236395000073 124.92514342100003,10.635391365000089 124.924952688000076,10.637418044000071 124.923190316000046,10.63837082200007 124.921488980000049,10.639503855000044 124.919528246000027,10.640710326000089 124.917651434000049,10.641472358000044 124.91602639000007,10.640281148000042 124.913745224000081,10.640371753000068 124.911769232000097,10.641294965000043 124.910815568000089,10.643343581000067 124.911593759000084,10.64568403700008 124.913020442000061,10.647188071000073 124.915133762000096,10.64721763600005 124.917445444,10.648070271000051 124.919352774,10.647461791000069 124.921366913000043,10.647271997000075 124.92326661300001,10.646363090000079 124.9254180800001,10.646624412000051 124.925837693000062,10.648776029000089 124.927600065000092,10.649884265000082";
-//        String n3 = "Kuapnit Balinsasayao";
-//        points.clear();
-//        points = generatePoints(s3);
-//        //Log.i(TAG, points.toString());
-//        createPoly(points, n3);
-//        try {
-//            //Scanner sc = new Scanner(file);
-//            //InputStream in = new BufferedInputStream(new FileInputStream(file));
-//            String line = null;
-//            FileReader reader = new FileReader(file);
-//            BufferedReader buff = new BufferedReader(reader);
-//            Log.i(TAG, "BEFORE THE WHILE LOOP");
-//            while ((line = reader.()) != null) {
-//                name = line;
-//                blankLine = line;
-//                buff.skip(line.length());
-//                line = buff.readLine();
-//                cords = line;
-//                Log.i(TAG, cords);
-//                //blankLine = sc.nextLine();
-//                points = generatePoints(cords);
-//                createPoly(points, name);
-//            }
-//        } catch (IOException e){
-//            // blah
-//            Log.i(TAG, "CAUGHT AN IO EXCEPTION");
-//            e.printStackTrace();
-//        } catch (Exception e){
-//            Log.i(TAG, "CAUGHT AN EXCEPTION");
-//            e.printStackTrace();
-//        }
+        String name, cords;
+
 
         final String[] names = getResources().getStringArray(R.array.names_array);
         final String[] locs = getResources().getStringArray(R.array.loc_array);
-        for (int i = 0; i < names.length; i++){
+        for (int i = 0; i < names.length; i++) {
             name = names[i];
             cords = locs[i];
             points = generatePoints(cords);
@@ -179,33 +159,83 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-//        String mLatitudeText = "", mLongitudeText = "";
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-//                    mGoogleApiClient);
-//            if (mLastLocation != null) {
-//                mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-//                mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
-//            }
-//            return;
-//        }
-
-
-
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
         // Add a marker in Sydney and move the camera
-        LatLng manila = new LatLng(14.5995, 120.9842);
-        mMap.addMarker(new MarkerOptions().position(manila).title("Marker in Manila"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(manila));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(14.5995, 120.9842),9.0f));
-
+        //LatLng manila = new LatLng(14.5995, 120.9842);
+        // mMap.addMarker(new MarkerOptions().position(manila).title("Marker in Manila"));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(manila));
+        // mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(14.5995, 120.9842),9.0f));
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(googleMap.getCameraPosition().zoom - 0.5f));
         //createPoly();
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
+        /*
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        */
+        Location location = null;
+
+        Log.i(TAG, "before permision check");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Request missing location permission.
+            Log.i(TAG, "missing permission");
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_LOCATION);
+        } else {
+            // Location permission has been granted, continue as usual.
+            Log.i(TAG, "already had permision");
+            //Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            location = locationManager.getLastKnownLocation(provider);
+            Log.i(TAG, "set location in else statement");
+        }
+
+
+        location = locationManager.getLastKnownLocation(provider);
+
+        if(location == null) {
+            Log.i(TAG, "location is null");
+            /*
+            LocationListener locationListener = new LocationListener() {
+                void onLocationChanged(Location location) {
+                    double lat = location.getLatitude();
+                    double lng = location.getLongitude();
+                    LatLng ll = new LatLng(lat, lng);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, 20));
+                }
+            };
+            */
+        }
+        else{
+            double lat= location.getLatitude();
+            double lng = location.getLongitude();
+
+            LatLng ll = new LatLng(lat, lng);
+            mMap.addMarker(new MarkerOptions().position(ll).draggable(true));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, 9));
+        }
+        //location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        //location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        //mMap.setMyLocationEnabled(true);
+        //Log.i(TAG, "before setting myLatLong");
+        //Log.i(TAG, "Lat: " + location.getLatitude());
+        //LatLng myLatLong = new LatLng(location.getLatitude(),location.getLongitude());
+        //mMap.addMarker(new MarkerOptions().position(myLatLong).title("My Location"));
+        //Log.i(TAG, "before camera zoom");
+        //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLatLong, 12.0f));
+        //Log.i(TAG, "after camera zoom");
         parse();
         //createHashMap();
 
@@ -216,5 +246,90 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Toast.makeText(MapsActivity.this, name, Toast.LENGTH_SHORT).show();
             }
         });
+
+        googleMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                //Toast.makeText(MapsActivity.this, marker.getPosition().toString(), Toast.LENGTH_SHORT).show();
+                boolean contained = false;
+                String name = "";
+                for(Map.Entry<Polygon, String> entry : hMap.entrySet()) {
+                    Polygon key = entry.getKey();
+                    String value = entry.getValue();
+                    if(PolyUtil.containsLocation(marker.getPosition(), key.getPoints(), false)){
+                        contained = true;
+                        name = value;
+                    }
+                }
+                if(contained){
+                    Toast.makeText(MapsActivity.this, "Entered a MPA", Toast.LENGTH_SHORT).show();
+
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(MapsActivity.this);
+                    builder1.setMessage("You have entered " + name  + ", a Marine Protected Area");
+                    builder1.setCancelable(true);
+
+                    builder1.setPositiveButton(
+                            "Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog alert = builder1.create();
+                    alert.show();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mClient.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://us.fishops.android.fishops/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(mClient, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://us.fishops.android.fishops/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mClient, viewAction);
+        mClient.disconnect();
     }
 }
