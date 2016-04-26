e6e7b9a39d4d78ca6d0e6ca39e4ce3d775e3d8b2
Gregory Greenleaf <pacohojaverde@gmail.com>
2016-04-25 23:56:06 -0700

    Wrapping up tonight's commits. :-)

--- app/src/main/java/us/fishops/android/fishops/MapsActivity.java
+++ app/src/main/java/us/fishops/android/fishops/MapsActivity.java
@@ -15,7 +15,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
